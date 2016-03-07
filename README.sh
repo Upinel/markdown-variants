ca226615424564507f7d9f92202b5cc769654395
KolenCheung <christian.kolen@gmail.com>
2016-03-07 00:33:13 -0800

    Added a bash to generate 4 versions of Markdown
    vanilla markdown, mmd, gfm, pandoc

---
+++ README.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/GitHub/markdown-cheatsheet
+multimarkdown README.md > README-mmd.html
+pandoc --mathjax -s -o README-pandoc.html README.md
+pandoc --mathjax -s -f markdown_strict -o README-md.html README.md
+pandoc --mathjax -s -f markdown_github -o README-gfm.html README.md
