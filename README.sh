083c8d39d043744da5b9f36ab9e016b6f330ea44
KolenCheung <christian.kolen@gmail.com>
2016-03-08 22:31:07 -0800

    Update the pandoc commands in the script

--- README.sh
+++ README.sh
@@ -2,8 +2,8 @@
 cd ~/GitHub/markdown-cheatsheet
 multimarkdown README.md > README-mmd.html
 pandoc -f markdown_strict -s -o README-md.html README.md
-pandoc -f markdown+mmd_header_identifiers+abbreviations+autolink_bare_uris+mmd_link_attributes+markdown_attribute+mmd_title_block+tex_math_double_backslash --smart --toc -s --mathjax -o README-pandoc.html README.md
-pandoc -f markdown+mmd_header_identifiers+abbreviations+autolink_bare_uris+mmd_link_attributes+markdown_attribute+mmd_title_block+tex_math_double_backslash --smart --toc -s -o README-pandoc.pdf README.md
+pandoc -f markdown+mmd_header_identifiers+abbreviations+autolink_bare_uris+mmd_link_attributes+markdown_attribute+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --mathjax -o README-pandoc.html README.md
+pandoc -f markdown+mmd_header_identifiers+abbreviations+autolink_bare_uris+mmd_link_attributes+markdown_attribute+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --latex-engine=pdflatex -V linkcolorblue -V citecolor=blue -V urlcolor=blue -V toccolor=blue -o README-pandoc.pdf README.md
 
 multimarkdown -t latex README.md > temp.tex
 latexmk -pdf temp.tex
