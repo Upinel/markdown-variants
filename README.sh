aa7c96897c1a7939b6acacac494f69abe6e6f95c
KolenCheung <christian.kolen@gmail.com>
2016-03-08 21:31:35 -0800

    Added PDF generations from pandoc and mmd

--- README.sh
+++ README.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd ~/GitHub/markdown-cheatsheet
 multimarkdown README.md > README-mmd.html
+pandoc -f markdown_strict -s -o README-md.html README.md
 pandoc --mathjax --toc -s -o README-pandoc.html README.md
-pandoc --mathjax -s -f markdown_strict -o README-md.html README.md
+pandoc --toc -s -o README-pandoc.pdf README.md
+
+multimarkdown -t latex README.md > temp.tex
+latexmk -pdf temp.tex
+mv temp.pdf README-mmd.pdf
+mv temp.tex ~/.Trash/
