87a00775e8e1c450007d13bb9e52c43d1f3479f2
KolenCheung <christian.kolen@gmail.com>
2016-03-12 16:16:51 -0800

    Update bash script to auto detect directory location

--- README.sh
+++ README.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ~/GitHub/markdown-cheatsheet
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 multimarkdown README.md > README-mmd.html
 pandoc -f markdown_strict -s -o README-md.html README.md
 pandoc -f markdown+mmd_header_identifiers+abbreviations+autolink_bare_uris+mmd_link_attributes+markdown_attribute+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --mathjax -o README-pandoc.html README.md
