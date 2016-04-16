0ecc348132f55de20aa599cdf4e7dacdfe453e57
KolenCheung <christian.kolen@gmail.com>
2016-04-16 15:06:29 -0700

    Update script script (functionally the same)

--- README.sh
+++ README.sh
@@ -9,13 +9,13 @@ multimarkdown --accept --reject README.md > README-mmd.html
 pandoc -f markdown_strict -s -o README-md.html README.md
 
 # pandoc markdown to XHTML
-pandoc -f markdown+mmd_header_identifiers+abbreviations+autolink_bare_uris+mmd_link_attributes+markdown_attribute+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --mathjax -t html -o README-pandoc-XHTML.html README.md
+pandoc -f markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --mathjax -t html -o README-pandoc-XHTML.html README.md
 
 # pandoc markdown to HTML5
-pandoc -f markdown+mmd_header_identifiers+abbreviations+autolink_bare_uris+mmd_link_attributes+markdown_attribute+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --mathjax -t html5 -o README-pandoc-HTML5.html README.md
+pandoc -f markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --mathjax -t html5 -o README-pandoc-HTML5.html README.md
 
 # pandoc markdown to LaTeX to PDF
-pandoc -f markdown+mmd_header_identifiers+abbreviations+autolink_bare_uris+mmd_link_attributes+markdown_attribute+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --latex-engine=pdflatex -V linkcolorblue -V citecolor=blue -V urlcolor=blue -V toccolor=blue -o README-pandoc.pdf README.md
+pandoc -f markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --latex-engine=pdflatex -V linkcolorblue -V citecolor=blue -V urlcolor=blue -V toccolor=blue -o README-pandoc.pdf README.md
 
 # MultiMarkdown to LaTeX to PDF
 multimarkdown --accept --reject -t latex README.md > temp.tex
