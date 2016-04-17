518561bbd183069c6610ab7dbe90ff17e7a0d8a7
KolenCheung <christian.kolen@gmail.com>
2016-04-17 14:44:21 -0700

    Added the generated intermediate tex files

--- README.sh
+++ README.sh
@@ -15,10 +15,12 @@ pandoc -f markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_heade
 pandoc -f markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --mathjax -t html5 -o README-pandoc-HTML5.html README.md
 
 # pandoc markdown to LaTeX to PDF
+pandoc -f markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --latex-engine=pdflatex -V linkcolorblue -V citecolor=blue -V urlcolor=blue -V toccolor=blue -o README-pandoc.tex README.md
 pandoc -f markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --latex-engine=pdflatex -V linkcolorblue -V citecolor=blue -V urlcolor=blue -V toccolor=blue -o README-pandoc.pdf README.md
 
 # MultiMarkdown to LaTeX to PDF
 multimarkdown --accept --reject -t latex README.md > temp.tex
 latexmk -pdf temp.tex
+mv temp.tex README-mmd.tex
 mv temp.pdf README-mmd.pdf
 find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
