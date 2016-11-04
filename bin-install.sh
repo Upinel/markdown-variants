2c42150179f7712f2a46fab760319151c276096f
Kolen Cheung <christian.kolen@gmail.com>
2016-11-04 03:37:27 -0700

    use `$HOME` instead of `~`

--- bin-install.sh
+++ bin-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-printf "%s\n" "" "# markdown-variants PATH" "export PATH=\"$DIR/bin:\$PATH\"" >> ~/.bash_profile
+printf "%s\n" "" "# markdown-variants PATH" "export PATH=\"$DIR/bin:\$PATH\"" >> $HOME/.bash_profile
